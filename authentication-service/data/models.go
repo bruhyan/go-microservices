ec8b5b0da37e30ea1ac66be5b418e8c754966947
bryan <bryankoh2196@gmail.com>
2024-05-04 13:18:29 +0800

    feat: db get user by id, update by id

--- authentication-service/data/models.go
+++ authentication-service/data/models.go
@@ -100,3 +100,69 @@ func (u *User) GetByEmail(email string) (*User, error) {
 	return &user, nil
 
 }
+
+// return one user by id
+func (u *User) GetOne(id string) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `select id, email, first_name, last_name, password, active, created_at, updated_at from users where id = $1`
+
+	row := db.QueryRowContext(ctx, query, id)
+
+	var user User
+
+	err := row.Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+		&user.Active,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		log.Println("Error scanning for user by id", err, id)
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+// update single user by receiver id
+func (u *User) Update() error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `update users set
+		email = $1,
+		first_name = $2,
+		last_name = $3,
+		active = $4,
+		updated_at = $5
+		where id = $6
+	`
+
+	_, err := db.ExecContext(ctx, query, u.Email, u.FirstName, u.LastName, u.Active, time.Now(), u.ID)
+
+	if err != nil {
+		log.Println("Error updating user by id", err, u.ID)
+		return err
+	}
+
+	return nil
+}
+
+// Delete single user by receiver id
+
+// Delete single user by id
+
+// insert new user, return id after insert
+
+// reset password
+
+// check password match using bcrypt
+// compare password input with hash stored for given user in the db
+// if password and hash match, return true, else false
